aa07fe39b2ebf6f959f1e33aab708f794c803985
Elliot Thomas <elliot@raziel.dev>
2021-01-18 15:51:28 +0000

    Welcome to the Machine

--- gmachine.go
+++ gmachine.go
@@ -4,3 +4,16 @@ package gmachine
 // DefaultMemSize is the number of 64-bit words of memory which will be
 // allocated to a new G-machine by default.
 const DefaultMemSize = 1024
+
+// Machine stores the details of a virtual CPU, gmachine.
+type Machine struct {
+	P      uint64
+	Memory []uint64
+}
+
+// New creates a new gmachine and returns it.
+func New() *Machine {
+	return &Machine{
+		Memory: make([]uint64, DefaultMemSize),
+	}
+}
